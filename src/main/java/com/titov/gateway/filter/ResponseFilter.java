cea71cb8fb16ae1b2d780e71a405e72b82b2ed67
anton <antontitow@bk.ru>
2024-09-18 04:30:48 +0300

    add tracking

---
+++ src/main/java/com/titov/gateway/filter/ResponseFilter.java
@@ -0,0 +1,33 @@
+package com.titov.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Configuration
+@Slf4j
+public class ResponseFilter {
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) -> {
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+                log.debug("Adding the correlation id to the outbound headers. {}");
+                if (requestHeaders.get(TrackingFilter.TRACK_ID) != null) {
+                    log.debug("TRACK ID " + requestHeaders.get(TrackingFilter.TRACK_ID).stream().findFirst().get());
+                    exchange.getResponse().getHeaders().add(TrackingFilter.TRACK_ID, UUID.randomUUID().toString());
+                } else {
+                    log.debug("NO TRACK ID");
+                    exchange.getResponse().getHeaders().add(TrackingFilter.TRACK_ID, UUID.randomUUID().toString());
+                }
+                log.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
+            }));
+        };
+    }
+}
---
+++ src/main/java/com/titov/gateway/filter/TrackingFilter.java
@@ -0,0 +1,35 @@
+package com.titov.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Component
+@Order(1)
+@Slf4j
+public class TrackingFilter implements GlobalFilter {
+    public static final String TRACK_ID = "track-id";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        val headers = exchange.getRequest().getHeaders();
+        if (headers.get(TRACK_ID) == null) {
+            log.debug("add track id");
+            exchange = exchange.mutate().request(exchange
+                            .getRequest()
+                            .mutate()
+                            .header(TRACK_ID, UUID.randomUUID().toString())
+                            .build())
+                    .build();
+        }
+
+        return chain.filter(exchange);
+    }
+}
